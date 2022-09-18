fb1f38e36a38da51e95db5013f5a664118577eef
Kawi <kawiwinter@gmail.com>
2022-09-18 16:45:08 +0200

    ver. 1 - shapes start forming

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 "use strict";
 
-const navbar = document.querySelector(".navigation");
+const navbar = document.querySelector(".navbar");
+const navBrand = document.querySelector(".navbar .navbar-brand");
+const navItem = document.querySelectorAll(".navbar-nav .nav-item");
 
 window.addEventListener("scroll", () => {
-  window.scrollY !== 0
-    ? (navbar.style.backgroundColor = "white")
-    : (navbar.style.backgroundColor = "rgba(0,0,0,0)");
+  if (window.scrollY !== 0) {
+    navbar.style.backgroundColor = "white";
+    navBrand.style.color = "black";
+    navItem.forEach((item) => (item.style.color = "black"));
+  } else {
+    navbar.style.backgroundColor = "rgba(0,0,0,0)";
+    navBrand.style.color = "#fec947";
+    navItem.forEach(
+      (item) => (item.style.color = "rgba(221, 221, 221, 0.826)")
+    );
+  }
 });
